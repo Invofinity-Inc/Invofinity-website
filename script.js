f92746d591b20dea13e683b12856f5f0b5abd62b
durgesh-k <dkudalkarsr2j@gmail.com>
2021-04-30 02:58:01 +0530

    UI ready

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+var click=false;
 var card=document.querySelector(".card1");
 var card2=document.querySelector(".card2");
 var card3=document.querySelector(".card3");
@@ -10,32 +10,55 @@ var route3=document.querySelector(".name33");
 
 
 card.addEventListener("mouseover",() => {
-    route.innerHTML="Discover more &nbsp;→"
+    route.style.opacity="1.0";
 });
 card.addEventListener("mouseleave",() => {
-    route.innerHTML=""
+    route.style.opacity="0.0";
 });
 
 card2.addEventListener("mouseover",() => {
-    route2.innerHTML="Discover more &nbsp;→"
+    route2.style.opacity="1.0";
 });
 card2.addEventListener("mouseleave",() => {
-    route2.innerHTML=""
+    route2.style.opacity="0.0";
 });
 
 card3.addEventListener("mouseover",() => {
-    route3.innerHTML="Discover more &nbsp;→"
+    route3.style.opacity="1.0";
 });
 card3.addEventListener("mouseleave",() => {
-    route3.innerHTML=""
+    route3.style.opacity="0.0";
 });
 
 
 //nav bar scroll effect---------------------------------------------------------------------------------------------------
 
 window.addEventListener("scroll",function(){
-    var nav=document.querySelector("nav");
+    var nav=document.querySelector(".nav");
+    var changelogo=document.querySelector(".logo");
+    var ham=document.querySelector(".bar1");
+    var ham1=document.querySelector(".bar2");
+    var ham2=document.querySelector(".bar3");
+    
     nav.classList.toggle("sticky",window.scrollY>0);
+    if(window.scrollY>0)
+    {
+        changelogo.src="assets/logo.svg";
+    }
+    else{
+        changelogo.src="assets/logo1.svg";
+    }
+    if(window.scrollY>0)
+    {
+        ham.style.backgroundColor="#000";
+        ham1.style.backgroundColor="#000";
+        ham2.style.backgroundColor="#000";
+    }
+    else{
+        ham.style.backgroundColor="#fff";
+        ham1.style.backgroundColor="#fff";
+        ham2.style.backgroundColor="#fff";
+    }
 });
 
 //end of nav bar scroll effect=======================================================================================
@@ -44,7 +67,7 @@ window.addEventListener("scroll",function(){
 //This below code enables nav bar effect-----------------------------------------------------------------------------------
 
 var lastId,
- topMenu = $(".items"),
+ topMenu = $(".nav-links"),
  topMenuHeight = topMenu.outerHeight()+1,
  menuItems = topMenu.find("a"),
  scrollItems = menuItems.map(function(){
@@ -85,3 +108,73 @@ $(window).scroll(function(){
 });
 
 //end of nav bar effect===============================================================================================
+
+var ex=document.querySelector(".ham");
+
+$(".ham").on("click", function(e) {
+    if(click){
+        click=false;
+    }
+    else{
+        click=true;
+    }
+    check(click);
+  });
+
+function check(value){
+    if(value)
+    {
+        $(".nav-links").css("height", "calc(100vh - 50px)");
+    $(".nav-links").css("overflow-y", "auto");
+    }
+    else{
+        $(".nav-links").css("height", "0");
+    }
+
+}
+
+$("li").on("click", function(e) {
+    if(click)
+    {
+        $(".nav-links").css("height", "0");
+        ex.classList.toggle("change");
+        click=false;
+    }
+  });
+
+
+  
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+
+
+
+  $(function(){  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('#hideme').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
